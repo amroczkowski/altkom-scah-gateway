4735e55e253db3a4050159f5127a628c35f88689
Artur Mroczkowski <a.mroczkowski@intive.com>
2022-06-19 10:50:45 +0200

    Exercise 4

---
+++ src/test/java/pl/altkom/scah/gateway/GatewayApplicationTests.java
@@ -0,0 +1,12 @@
+package pl.altkom.scah.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
